0ef44c0fd695892f3d1ef1f098acfbf0fc6d70a5
Felipe Guerra <sixto.guerra1982@gmail.com>
2022-07-04 20:47:46 -0400

    kaminari

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,16 @@ class Post < ApplicationRecord
     validates :author, presence: true
 
     enum state: [:draft, :published]
+
+    def self.load
+        30.times do |i|
+            Post.create(
+                title: "Titulo #{i+1}",
+                description: "Description" * (i + i) ,
+                author: "Sixto",
+                state: "published"
+            )
+            puts "Creando el registro #{i+1}"
+        end
+    end
 end
